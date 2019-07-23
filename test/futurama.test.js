78f058b39d067a69210d425506b4c0189a5142da
DannySuarez <suarezd10@gmail.com>
2019-07-22 20:18:06 -0700

    chaneged route path

--- test/futurama.test.js
+++ test/futurama.test.js
@@ -1,17 +1,15 @@
 const request = require('supertest');
-const app = require('../lib/app')
+const app = require('../lib/app');
 
 describe('futurama', () => {
   it('creates profile with POST', () => {
     return request(app)
-      .post('/')
+      .post('/profile')
       .send({
         name: 'danny',
         favoriteCharacter: 'Bender'
       })
       .then(res => {
-        console.log(res.body);
-        
         expect(res.body).toEqual({
           name: 'danny',
           favoriteCharacter: 'Bender',
